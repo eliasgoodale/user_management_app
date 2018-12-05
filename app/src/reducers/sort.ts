0154a6915031bfd2def17bf97564574c30b88553
snugitup <eligoodale@yahoo.com>
2018-12-04 20:54:10 -0800

    tweaked sorting behavior on users/CHANGE_DATA dispatch

--- app/src/reducers/sort.ts
+++ app/src/reducers/sort.ts
@@ -8,6 +8,8 @@ export default (state: SortDescriptor[] = initialState, action: any) => {
     switch(action.type) {
         case 'users/CHANGE_SORT':
             return action.payload;
+        case 'users/CHANGE_DATA':
+            return [];
         default:
             return state;
     }
